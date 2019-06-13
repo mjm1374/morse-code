484c7ef378a279e1a941bd7ec8e6271561fb50d8
Mike McAllister <mike@logikbox.com>
2019-06-13 19:08:56 -0400

    gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+//var cssnano = require('gulp-cssnano');
+//var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify-es').default;
+//var sourcemaps =  require('gulp-sourcemaps');
+//var autoprefixer = require('gulp-autoprefixer');
+//var sassdoc = require('sassdoc');
+
+var concatCss = require('gulp-concat-css');
+ 
+gulp.task('css', function () {
+  return gulp.src('css/**/*.css')
+    .pipe(concatCss("styles/bundle.css"))
+    .pipe(gulp.dest(''));
+});
+
+// gulp.task('sass', function () {
+//     return gulp.src('scss/*.scss')
+//         .pipe(sourcemaps.init())
+//         .pipe(sass())
+//         .pipe(sourcemaps.write())
+//         .pipe(autoprefixer())
+//         .pipe(cssnano())
+//         .pipe(gulp.dest('css'))
+//         .pipe(sassdoc())
+//         // Release the pressure back and trigger flowing mode (drain)
+//         // See: http://sassdoc.com/gulp/#drain-event
+//         .resume();
+// });
+
+gulp.task('js', function () {
+    return gulp.src(['js/*.js'])
+        .pipe(concat('script.min.js'))
+        .on('error', onError)
+        .pipe(uglify())
+        .on('error', onError)
+        .pipe(gulp.dest('javascript'));
+});
+
+gulp.task('watch', function () {
+    //gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('javascript/*.js', ['js']);
+    gulp.watch('css/*.css', ['css']);
+});
+
+gulp.task('default', ['js', 'css', 'watch']);
+
+function onError(err) {
+    console.log(err);
+    this.emit('end');
+  }
